68a77e2c84db23eaae89e80a1cd733dd5ab579c7
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:15:50 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -53,3 +53,4 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
